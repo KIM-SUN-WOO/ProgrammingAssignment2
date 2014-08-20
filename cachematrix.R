f6a10018453a922aa49f555cbb244da6d524a03d
KIM SUN WOO <swkim@kbs.co.kr>
2014-08-20 14:31:04 +0900

    Programming assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -3,9 +3,14 @@
 
 ## Write a short comment describing this function
 
-## variables in the closure of the 4 functions.
+## Variables for use in the closure of the 4 functions.
+##   ( the functions are like methods in object oriented sense )
+
 ## x : matrix
 ## i : inversion of the matrix. if it is null, it is not computed yet.
+## When we call makeCacheMatrix with a parameter which is a matrix,
+##  set(), get(), setinv() and getinv() functions are defined with the
+##  variables in their closure environment.
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -27,19 +32,33 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Making use of the cache to compute the inverse of the matrix.
+##
+## Check first whether the inversion was already computed or not.
+## If there is in the cache, use it.
+## Otherwise, compute the inverse of the matrix.
 
 cacheSolve <- function(x, ...) {
+
         ## Return a matrix that is the inverse of 'x'
 
 	i <- x$getinv()
+
+	## Check whether the inversion of the matrix exists or not
+	## If it is there, return it.
 	if(!is.null(i)) {
 		message("getting cached data")
 		return(i)
 	}
+
+	## The inversion of the matrix was not computed yet.
+	##  So, get the matrix
 	data <- x$get()
 
+	##  Then compute the inverse of the matrix
 	i <- solve(data, ... )
+
+	##  Finally, save it for later use.
 	x$setinv(i)
 
 	i
