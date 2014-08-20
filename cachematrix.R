a0b631af3fc0a98cc60daed26b59dfe5ef143143
KIM SUN WOO <swkim@kbs.co.kr>
2014-08-20 14:01:06 +0900

    programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,26 @@
 
 ## Write a short comment describing this function
 
+## variables in the closure of the 4 functions.
+## x : matrix
+## i : inversion of the matrix. if it is null, it is not computed yet.
+
 makeCacheMatrix <- function(x = matrix()) {
+
+	i <- NULL
+
+	set <- function(y) {
+		x <<- y
+		i <<- NULL
+	}
+
+	get <- function() x
+
+	setinv <- function(ii) i <<- ii
+
+	getinv <- function() i
+
+	list(set=set, get=get, setinv = setinv, getinv = getinv)
 
 }
 
@@ -12,4 +31,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+	i <- x$getinv()
+	if(!is.null(i)) {
+		message("getting cached data")
+		return(i)
+	}
+	data <- x$get()
+
+	i <- solve(data, ... )
+	x$setinv(i)
+
+	i
 }
